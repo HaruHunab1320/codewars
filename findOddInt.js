150a6761863372daf7532e99ed16928c89da8151
Jakob <jakobmsgrant@gmail.com>
2020-06-25 21:18:10 -0700

    Completed find the odd int

--- findOddInt.js
+++ findOddInt.js
@@ -1,15 +1,18 @@
 //find the integer that appears an odd number of times
 function findOdd(A) {
 
+//define an empty object
   let counts = {};
 
+//loop through array, count occurences of a value 
   for (var i = 0; i < A.length; i++) {
     let num = A[i];
     counts[num] = counts[num] ? counts[num] + 1 : 1;
   }
 
+//search the values for the number that doesnt factor to 0
   let result = Object.keys(counts).find(key => counts[key] % 2 > 0);
   
+  //parse the key value to turn the key string into an integer
   return parseInt(result);
-
 }
